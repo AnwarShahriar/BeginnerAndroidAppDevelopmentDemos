ebf86a06185e7ee30b4addfcc53bf62fcb9f025e
anwarshahriar <anwarshahriar@gmail.com>
2017-04-16 20:42:00 +0600

    EditText demo

--- EventListener/app/src/main/java/me/anwarshahriar/eventlistener/MainActivity.java
+++ EventListener/app/src/main/java/me/anwarshahriar/eventlistener/MainActivity.java
@@ -5,14 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText fieldName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fieldName = (EditText) findViewById(R.id.field_name);
 
         Button buttonSubmit = (Button) findViewById(R.id.button_submit);
         buttonSubmit.setOnClickListener(this);
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        Toast.makeText(this, "blablabla", Toast.LENGTH_SHORT).show();
+        String name = fieldName.getText().toString();
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
     }
 }
