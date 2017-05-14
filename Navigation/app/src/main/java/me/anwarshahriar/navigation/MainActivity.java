23330b2d544d8d4c6f5ccee9f7df383a4a5f49e3
anwarshahriar <anwarshahriar@gmail.com>
2017-05-14 19:23:01 +0600

    Added view pager demo

---
+++ Navigation/app/src/main/java/me/anwarshahriar/navigation/MainActivity.java
@@ -0,0 +1,60 @@
+package me.anwarshahriar.navigation;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabsHolder;
+    ViewPager pager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+
+        tabsHolder = (TabLayout) findViewById(R.id.tabs_holder);
+        tabsHolder.setupWithViewPager(pager);
+    }
+
+    class FragmentAdapter extends FragmentPagerAdapter {
+
+        public FragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new FragmentA();
+                case 1:
+                    return new FragmentB();
+                case 2:
+                    return new FragmentC();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Tab " + position;
+        }
+    }
+}
